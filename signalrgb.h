c4609957d6eb657106978c14fb697f494e91d150
Szymon <szymek.fogiel@gmail.com>
2026-01-18 19:42:52 +0100

    Format

--- signalrgb.h
+++ signalrgb.h
@@ -1,29 +1,17 @@
 #pragma once
 
-enum signalrgb_commands
-{
-    GET_QMK_VERSION = 0x21,
-    GET_PROTOCOL_VERSION = 0x22,
-    GET_UNIQUE_IDENTIFIER = 0x23,
-    STREAM_RGB_DATA = 0x24,
-    SET_SIGNALRGB_MODE_ENABLE = 0x25,
+enum signalrgb_commands {
+    GET_QMK_VERSION            = 0x21,
+    GET_PROTOCOL_VERSION       = 0x22,
+    GET_UNIQUE_IDENTIFIER      = 0x23,
+    STREAM_RGB_DATA            = 0x24,
+    SET_SIGNALRGB_MODE_ENABLE  = 0x25,
     SET_SIGNALRGB_MODE_DISABLE = 0x26,
-    GET_TOTAL_LEDS = 0x27,
-    GET_FIRMWARE_TYPE = 0x28,
+    GET_TOTAL_LEDS             = 0x27,
+    GET_FIRMWARE_TYPE          = 0x28,
 };
 
-enum signalrgb_responses
-{
-    PROTOCOL_VERSION_BYTE_1 = 1,
-    PROTOCOL_VERSION_BYTE_2 = 0,
-    PROTOCOL_VERSION_BYTE_3 = 6,
-    DEVICE_UNIQUE_IDENTIFIER_BYTE_1 = 0,
-    DEVICE_UNIQUE_IDENTIFIER_BYTE_2 = 0,
-    DEVICE_UNIQUE_IDENTIFIER_BYTE_3 = 0,
-    FIRMWARE_TYPE_BYTE = 2,
-    DEVICE_ERROR_LED_BOUNDS = 253,
-    DEVICE_ERROR_LED_COUNT = 254 
-};
+enum signalrgb_responses { PROTOCOL_VERSION_BYTE_1 = 1, PROTOCOL_VERSION_BYTE_2 = 0, PROTOCOL_VERSION_BYTE_3 = 6, DEVICE_UNIQUE_IDENTIFIER_BYTE_1 = 0, DEVICE_UNIQUE_IDENTIFIER_BYTE_2 = 0, DEVICE_UNIQUE_IDENTIFIER_BYTE_3 = 0, FIRMWARE_TYPE_BYTE = 2, DEVICE_ERROR_LED_BOUNDS = 253, DEVICE_ERROR_LED_COUNT = 254 };
 
 void get_qmk_version(void);
 void get_signalrgb_protocol_version(void);
@@ -36,13 +24,13 @@ void signalrgb_mask_key(uint8_t index);
 void signalrgb_unmask_key(uint8_t index);
 void signalrgb_sync_mask(const bool *mask);
 
-//Changelogs for Firmware Versions------------------------------------
-//V1.0.1 added detection for the total number of LEDs a board has. Plugins need a rewrite to make use of this change. Rewritten plugins will not function with older firmware.
-//V1.0.2 added another detection byte for which fork of SignalRGB firmware device is running. This means we can keep overlap Unique Identifiers between Sonix and Mainline.
-//V1.0.3 unifies the command IDs between Mainline, Sonix, and VIA. All commands have a 0x20 offset now.
-//V1.0.4 improves detection handling, and has a complete rewrite of the plugins. Also merges Mainline and VIA branches. VIA Branch is deprecated.
-//V1.0.5 adds support for RGBLight and adds proper bounds checking for leds to ensure we don't crash the firmware.
-//V1.0.6 fixes conflict with VIA running at the same time.
+// Changelogs for Firmware Versions------------------------------------
+// V1.0.1 added detection for the total number of LEDs a board has. Plugins need a rewrite to make use of this change. Rewritten plugins will not function with older firmware.
+// V1.0.2 added another detection byte for which fork of SignalRGB firmware device is running. This means we can keep overlap Unique Identifiers between Sonix and Mainline.
+// V1.0.3 unifies the command IDs between Mainline, Sonix, and VIA. All commands have a 0x20 offset now.
+// V1.0.4 improves detection handling, and has a complete rewrite of the plugins. Also merges Mainline and VIA branches. VIA Branch is deprecated.
+// V1.0.5 adds support for RGBLight and adds proper bounds checking for leds to ensure we don't crash the firmware.
+// V1.0.6 fixes conflict with VIA running at the same time.
 
 /* keymap default:
 DEVICE, DEVICE_UNIQUE_IDENTIFIER_BYTE_1,DEVICE_UNIQUE_IDENTIFIER_BYTE_2,DEVICE_UNIQUE_IDENTIFIER_BYTE_3
@@ -64,7 +52,6 @@ redragon/k580:default                       0 1 4
 redragon/k582:default                       0 1 3
 redragon/k630:default                       0 1 5
 */
-
 
 /* keymap via:
 DEVICE, DEVICE_UNIQUE_IDENTIFIER_BYTE_1,DEVICE_UNIQUE_IDENTIFIER_BYTE_2,DEVICE_UNIQUE_IDENTIFIER_BYTE_3
